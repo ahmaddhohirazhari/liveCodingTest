04d82432f15023c22468835c654742a85cdc0cc3
ahmad dhohir azhari <dhohir12@gmail.com>
2022-11-22 13:46:19 +0700

    update

--- liveCodingTest.js
+++ liveCodingTest.js
@@ -19,10 +19,10 @@ function pairNumber(arr) {
   return res;
 }
 console.log("NO 1");
-console.log(cntgloves([5, 7, 7, 9, 10, 4, 5, 10, 6, 5]));
-console.log(cntgloves([10, 20, 20, 10, 10, 30, 50, 10, 20]));
-console.log(cntgloves([6, 5, 2, 3, 5, 2, 2, 1, 1, 5, 1, 3, 3, 3, 5]));
-console.log(cntgloves([1, 1, 3, 1, 2, 1, 3, 3, 3, 3]));
+console.log(pairNumber([5, 7, 7, 9, 10, 4, 5, 10, 6, 5]));
+console.log(pairNumber([10, 20, 20, 10, 10, 30, 50, 10, 20]));
+console.log(pairNumber([6, 5, 2, 3, 5, 2, 2, 1, 1, 5, 1, 3, 3, 3, 5]));
+console.log(pairNumber([1, 1, 3, 1, 2, 1, 3, 3, 3, 3]));
 console.log("===============================================");
 
 function checkLetter(sentence) {
